23ef84e8faddfa5eece65817a79abc21ec49475d
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2024-09-05 16:38:45 +0900

    Update Circle.java

--- src/Circle.java
+++ src/Circle.java
@@ -1,4 +1,19 @@
 package PACKAGE_NAME;
 
 public class Circle {
+    private double radius;
+
+    //Constructor
+    public Circle(double radius) {
+        this.radius = (radius < 0 ? 0: radius); //Using ternary operator
+    }
+
+    public double getRadius(){
+        return this.radius;
+    }
+
+    public double getArea(){
+        return this.radius * this.radius * Math.PI;
+    }
+  
 }
