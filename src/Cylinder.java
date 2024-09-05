04e884ca83070d5fc6cba36bd92a33d277d15dbc
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2024-09-05 16:39:05 +0900

    Update Cylinder.java

--- src/Cylinder.java
+++ src/Cylinder.java
@@ -1,4 +1,18 @@
 package PACKAGE_NAME;
 
 public class Cylinder {
+    private double height;
+
+    public Cylinder(double radius, double height) {
+        super(radius);
+        this.height = (height < 0 ? 0: height);
+    }
+
+    public double getHeight() {
+        return this.height;
+    }
+
+    public double getVolume(){
+        return super.getArea() * this.height;
+    }
 }
